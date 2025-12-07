9a6bba1870f01f6cdd76385fbc2b63289bdcc099
DanaTryon <dt463@njit.edu>
2025-12-06 22:35:06 -0500

    fixing workflow testing

--- app/database.py
+++ app/database.py
@@ -1,9 +1,15 @@
 # app/database.py
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
+# For SQLite, need connect_args
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Add Base for models to inherit from
+Base = declarative_base()
